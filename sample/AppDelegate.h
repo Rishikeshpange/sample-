3bf2fadd468e7486e932c8deb7d758a521028d85
rishi0312 <rishikesh.pange@tatatechnologies.com>
2016-03-10 12:58:57 +0530

    Initial Commit

---
+++ sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sample
+//
+//  Created by Rishikesh Pange on 3/10/16.
+//  Copyright © 2016 TTL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sample
+//
+//  Created by Rishikesh Pange on 3/10/16.
+//  Copyright © 2016 TTL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
